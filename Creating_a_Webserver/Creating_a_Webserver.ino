c6aeb5c2b6a87187097a8d1ad45b04a9913e452a
desairahulinrd000 <56038822+desairahulinrd000@users.noreply.github.com>
2020-05-23 13:35:10 +0530

    Initial commit

---
+++ Creating_a_Webserver/Creating_a_Webserver.ino
@@ -0,0 +1,33 @@
+/*Creating a webserver Using ESP8266*/
+#include<ESP8266WiFi.h>
+#include<ESP8266WebServer.h>
+#include<DHT.h>
+ESP8266WebServer server(80);
+const char* ssid="YOUR_SSID";
+const char* pwd="YOUR_PASSWORD";
+String website; 
+void websitecontent()
+{
+  website="<html><body>";
+  website="HelloWorld";
+  website+="</body></html>";
+  server.send(200,"text/html",website);
+}
+void setup()
+{
+  WiFi.begin(ssid,pwd);
+  while(WiFi.status()!=WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(1000);
+  }
+  WiFi.mode(WIFI_STA);
+  Serial.println("Connected");
+  Serial.println(WiFi.localIP());
+  server.begin();
+  server.on("/",websitecontent);
+}
+void loop()
+{
+  server.handleClient();
+}
